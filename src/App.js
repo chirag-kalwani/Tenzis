b724f94cdbd35f69fe9b1e8e9d6d4f5cb8482732
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2022-11-03 23:00:02 +0530

    confetti inserted

--- src/App.js
+++ src/App.js
@@ -1,72 +1,73 @@
 import React from "react";
 import Die from "./Die.js";
 import "./style.css"
-export default function App(){
-    
-    const [randNumbers, setRandNumbers] = React.useState(allNewDice());
-    const [tenzis, setTenzis] = React.useState(false);
-    function allNewDice(){
-        let ret = [];
-        for (let i = 0;i<10;i++){
-            ret.push(
-                {
-                    value: Math.floor(Math.random() * 6 + 1),
-                    isHold: false
-            });
-        }
-        return ret;
-    }
-    function rollDice(){
-        
-        setRandNumbers(prevState=>{
-            let newRandomNumbers = allNewDice();
-            return prevState.map( (ele, i) => ele.isHold ? ele : newRandomNumbers[i] );
+import Confetti from 'react-confetti'
+export default function App() {
+
+  const [randNumbers, setRandNumbers] = React.useState(allNewDice());
+  const [tenzis, setTenzis] = React.useState(false);
+  function allNewDice() {
+    let ret = [];
+    for (let i = 0; i < 10; i++) {
+      ret.push(
+        {
+          value: Math.floor(Math.random() * 6 + 1),
+          isHold: false
         });
     }
-    function handleClick(id){
-        setRandNumbers(prevState => prevState.map((ele, i)=>{
-                let newEle = ele;
-                if(id === i)
-                    newEle.isHold = !newEle.isHold;
-                return newEle;
-            })
-        )
-    }
-    function restart(){
-        setRandNumbers(allNewDice());
-        setTenzis(false);
-    }
-    React.useEffect(()=>{
-        const isHold = randNumbers.every(die => die.isHold);
-        const firstValue = randNumbers[0].value;
-        const allSameValue = randNumbers.every(die => die.value === firstValue);
-        setTenzis(isHold && allSameValue);
-    }, [randNumbers])
-    return (
-        <main>
-                <h1 className="title">Tenzies</h1>
-                <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-                <div className="container">
-                {
-                    randNumbers.map((randNumber, i) => (
-                        <Die 
-                            key={i}
-                            index={i}
-                            value={randNumber.value} 
-                            isHold={randNumber.isHold}
-                            onClick={handleClick}
-                        />
-                    ))
-                }
-                
-            </div>
-            {!tenzis && <button onClick={rollDice} className="roll">Roll</button>}
-            {tenzis && (  
-                    <>
-                        <h3> You Won </h3>
-                        <button onClick={restart} className="roll">Restart Game</button>
-                    </>
-                )}
-        </main>
+    return ret;
+  }
+  function rollDice() {
+    setRandNumbers(prevState => {
+      let newRandomNumbers = allNewDice();
+      return prevState.map((ele, i) => ele.isHold ? ele : newRandomNumbers[i]);
+    });
+  }
+  function handleClick(id) {
+    setRandNumbers(prevState => prevState.map((ele, i) => {
+      let newEle = ele;
+      if (id === i)
+        newEle.isHold = !newEle.isHold;
+      return newEle;
+    })
     )
+  }
+  function restart() {
+    setRandNumbers(allNewDice());
+    setTenzis(false);
+  }
+  React.useEffect(() => {
+    const isHold = randNumbers.every(die => die.isHold);
+    const firstValue = randNumbers[0].value;
+    const allSameValue = randNumbers.every(die => die.value === firstValue);
+    setTenzis(isHold && allSameValue);
+  }, [randNumbers])
+  return (
+    <main>
+      {tenzis && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="container">
+        {
+          randNumbers.map((randNumber, i) => (
+            <Die
+              key={i}
+              index={i}
+              value={randNumber.value}
+              isHold={randNumber.isHold}
+              onClick={handleClick}
+            />
+          ))
+        }
+
+      </div>
+      {!tenzis && <button onClick={rollDice} className="roll">Roll</button>}
+      {tenzis && (
+        <>
+          <h3> You Won </h3>
+          <button onClick={restart} className="roll">Restart Game</button>
+        </>
+      )}
+    </main>
+  )
 }
--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,13 @@
 import React from "react";
-export default function Die(props){
-    return (
-        <div className="die"
-             style={{backgroundColor: props.isHold?"#59E391":"white"}}
-             onClick={() => props.onClick(props.index)}
-        >
-            <h3>
-                {props.value}
-            </h3>
-        </div>
-    )
+export default function Die(props) {
+  return (
+    <div className="die"
+      style={{ backgroundColor: props.isHold ? "#59E391" : "white" }}
+      onClick={() => props.onClick(props.index)}
+    >
+      <h1>
+        {props.value}
+      </h1>
+    </div>
+  )
 }
